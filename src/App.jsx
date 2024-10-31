1d0621141e97ee154c5331f9e754f76440cf5464
aldoLorenzio <aldolorenzio@gmail.com>
2024-10-31 21:00:37 +0700

    add: assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-        <h1 className='text-5xl text-red-500'>Testing</h1>
+      <h1 className="text-5xl text-red-400">Testing</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
